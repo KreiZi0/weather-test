5404d9aee6eea9c935891973fa5506eb6dbd7044
KreiZi0 <steven.mirontsuk1@gmail.com>
2024-02-21 10:08:40 +0200

    Weather tests in work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Search from './components/Search';
+import WeatherCard from './components/WeatherCard';
+import { createMockServer } from './createMockServer';
 
-function App() {
+if (process.env.NODE_ENV === 'development') {
+  createMockServer();
+}
+
+const WeatherApplication = () => {
+  const [selected, setSelected] = useState([]);
+
+  const selectCity = (city) => {
+    setSelected([...selected, city]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Weather Application</h1>
+      <Search onSelectItem={selectCity} />
+
+      <div data-testid="my-weather-list">
+        {selected && selected.map((city) =>
+          <div key={`${city.lat}-${city.lon}`}>
+            <WeatherCard city={city} />
+          </div>
+      )}
+</div>
     </div>
   );
 }
 
-export default App;
+export default WeatherApplication;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,110 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, act, within } from '@testing-library/react';
 import App from './App';
+import { createMockServer } from './createMockServer';
+import userEvent from '@testing-library/user-event';
+import WeatherCard from './components/WeatherCard';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let server;
+
+beforeEach(() => {
+  server = createMockServer();
+});
+
+afterEach(() => {
+  server.shutdown();
+});
+
+describe('Weather Application tests', () => {
+  it('renders weather application title', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Weather Application/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('shows city search results', async () => {
+    render(<App />);
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne');
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5));
+  });
+
+  it('shows city search result details', async () => {
+    render(<App />);
+    
+    // Use queryAllByText to find all elements containing "Melbourne"
+    const searchResults = screen.queryAllByText(/Melbourne/i);
+  
+    // Iterate through the search results
+    searchResults.forEach((result) => {
+      const parent = result.closest('.search-result');
+      if (parent) {
+        // Verify that the parent element contains latitude and longitude
+        expect(within(parent).getByText('-37.8141705, 144.9655616')).toBeInTheDocument();
+      }
+    });
+  });
+  
+  
+  
+  
+  
+  
+
+  it('add search result to my weather list', async () => {
+    render(<App />);
+  
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne');
+  
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+  
+    await waitFor(() => {
+      expect(screen.getByTestId('search-results')).toBeInTheDocument();
+    });
+  
+    const selected = screen.getAllByText(/Melbourne/i)[3];
+    act(() => {
+      userEvent.click(selected);
+    });
+  
+    // Wait for the search results to disappear
+    await waitFor(() => {
+      expect(screen.queryByTestId('search-results')).not.toBeInTheDocument();
+    });
+  
+    expect(within(screen.getByTestId('my-weather-list')).getByText(/Melbourne/i)).toBeInTheDocument();
+  });
+  
+  
+});
+
+describe('WeatherCard component tests', () => {
+  it('renders city name', () => {
+    const city = {
+      name: 'Melbourne',
+      country: 'Australia',
+      state: 'Victoria',
+      lat: 0,
+      lon: 0,
+    };
+    render(<WeatherCard city={city}></WeatherCard>);
+    expect(screen.getByText(city.name)).toBeInTheDocument();
+  });
+
+  it('renders temperature', async () => {
+    const city = {
+      name: 'Melbourne',
+      country: 'Australia',
+      state: 'Victoria',
+      lat: 0,
+      lon: 0,
+    };
+    render(<WeatherCard city={city}></WeatherCard>);
+    await waitFor(() => expect(screen.getByText(19.53)).toBeInTheDocument()) ;
+  });
 });
